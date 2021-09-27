32dec985941028b2fbc6c46b602fef6eaaca622c
Rachael <mcbride.rac@gmail.com>
2021-09-27 17:41:01 -0400

    Passes waves 1 and 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,6 @@
 class Item:
-    pass
+    def __init__(self, category=""):
+        self.category = category
+
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,32 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item_to_add):
+        self.inventory.append(item_to_add)
+        return item_to_add
+    
+    def remove(self, item_to_remove):
+        if item_to_remove in self.inventory:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+        else:
+            return False 
+    
+    def get_by_category(self, category):
+        items_by_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_by_category.append(item)
+        return items_by_category 
+
+    def swap_items(self, friend, my_item, their_item):
+        pass 
+        # self.add(their_item)
+        # self.remove(my_item)
+        # friend.add(my_item)
+        # friend.remove(their_item)
+        # if my_item not in self.inventory or their_item not in friend.inventory:
+        #     return False 
+        # return True 
+        
