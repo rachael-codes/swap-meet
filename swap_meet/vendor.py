f589731be9289da666755f48858c258e13d4b087
Rachael <mcbride.rac@gmail.com>
2021-09-27 20:32:16 -0400

    Passes all test waves

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -37,3 +37,79 @@ class Vendor:
 
         self.swap_items(friend, self.inventory[0], friend.inventory[0])
         return True 
+
+    def get_best_by_category(self, category):
+        best_item, rating = "", -1
+        for item in self.inventory:
+            if item.category == category:
+                if item.condition > rating:
+                    best_item, rating = item, item.condition
+
+        if best_item == "":
+            return None 
+        else:
+            return best_item 
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = other.get_best_by_category(my_priority)
+        their_best_item = self.get_best_by_category(their_priority)
+
+        if my_best_item == None or their_best_item == None:
+            return False 
+            
+        self.swap_items(other, their_best_item, my_best_item)
+        return True 
+# -----------------------------------------------------------------------------
+
+class Item:
+    def __init__(self, category="", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Yikes!"
+        elif self.condition == 1:
+            return "Why would you want this?"
+        elif self.condition == 2:
+            return "Ugh"
+        elif self.condition == 3:
+            return "Passable"
+        elif self.condition == 4:
+            return "Not bad"
+        elif self.condition == 5:
+            return "Great find!"
+        
+
+class Clothing(Item):
+    def __init__(self, condition=0):
+        self.category = "Clothing"
+        self.condition = condition
+
+    def __str__(self):
+        return "The finest clothing you could wear."
+
+class Decor(Item):
+    def __init__(self, condition=0):
+        self.category = "Decor"
+        self.condition = condition
+
+    def __str__(self):
+        return "Something to decorate your space."
+
+# item_a = Decor(condition=2.0)
+# item_b = Electronics(condition=4.0)
+# item_c = Decor(condition=4.0)
+# tai = Vendor(inventory=[item_a, item_b, item_c])
+    
+# item_d = Clothing(condition=2.0)
+# item_e = Decor(condition=4.0)
+# item_f = Clothing(condition=4.0)
+# jesse = Vendor(inventory=[item_d, item_e, item_f])
+    
+# result = tai.swap_best_by_category(other=jesse, my_priority="Clothing", their_priority="Decor")
+# >       assert result
+# E       assert False
