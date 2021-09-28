85ff4af353b1a09f7351ed2fdd774808d114878e
Rachael <mcbride.rac@gmail.com>
2021-09-27 20:43:57 -0400

    Add optional enhancements for swapping newest item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, category="", condition=0):
+    def __init__(self, category="", condition=0, age=0):
         self.category = category
         self.condition = condition
+        self.age = age
 
     def __str__(self):
         return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,19 +24,19 @@ class Vendor:
         if my_item not in self.inventory or \
             their_item not in friend.inventory:
             return False 
-
-        self.remove(my_item)
-        friend.add(my_item)
-        friend.remove(their_item)
-        self.add(their_item)
-        return True 
+        else: 
+            self.remove(my_item)
+            friend.add(my_item)
+            friend.remove(their_item)
+            self.add(their_item)
+            return True 
 
     def swap_first_item(self, friend):
         if len(self.inventory) == 0 or len(friend.inventory) == 0:
             return False 
-
-        self.swap_items(friend, self.inventory[0], friend.inventory[0])
-        return True 
+        else:
+            self.swap_items(friend, self.inventory[0], friend.inventory[0])
+            return True 
 
     def get_best_by_category(self, category):
         best_item, rating = "", -1
@@ -56,60 +56,27 @@ class Vendor:
 
         if my_best_item == None or their_best_item == None:
             return False 
-            
-        self.swap_items(other, their_best_item, my_best_item)
+        else:  
+            self.swap_items(other, their_best_item, my_best_item)
+            return True 
+
+def get_newest_item(self):
+    newest_item, age = "", 1000000
+    for item in self.inventory:
+        if item.age < age:
+            newest_item, age = item, item.age 
+    
+    if newest_item == "":
+        return None 
+    else:
+        return newest_item
+
+def swap_by_newest(self, other):
+    my_newest_item = self.get_newest_item()
+    their_newest_item = other.get_newest_item()
+
+    if my_newest_item == None or their_newest_item == None:
+        return False 
+    else: 
+        self.swap_items(other, their_newest_item, my_newest_item)
         return True 
-# -----------------------------------------------------------------------------
-
-class Item:
-    def __init__(self, category="", condition=0):
-        self.category = category
-        self.condition = condition
-
-    def __str__(self):
-        return "Hello World!"
-    
-    def condition_description(self):
-        if self.condition == 0:
-            return "Yikes!"
-        elif self.condition == 1:
-            return "Why would you want this?"
-        elif self.condition == 2:
-            return "Ugh"
-        elif self.condition == 3:
-            return "Passable"
-        elif self.condition == 4:
-            return "Not bad"
-        elif self.condition == 5:
-            return "Great find!"
-        
-
-class Clothing(Item):
-    def __init__(self, condition=0):
-        self.category = "Clothing"
-        self.condition = condition
-
-    def __str__(self):
-        return "The finest clothing you could wear."
-
-class Decor(Item):
-    def __init__(self, condition=0):
-        self.category = "Decor"
-        self.condition = condition
-
-    def __str__(self):
-        return "Something to decorate your space."
-
-# item_a = Decor(condition=2.0)
-# item_b = Electronics(condition=4.0)
-# item_c = Decor(condition=4.0)
-# tai = Vendor(inventory=[item_a, item_b, item_c])
-    
-# item_d = Clothing(condition=2.0)
-# item_e = Decor(condition=4.0)
-# item_f = Clothing(condition=4.0)
-# jesse = Vendor(inventory=[item_d, item_e, item_f])
-    
-# result = tai.swap_best_by_category(other=jesse, my_priority="Clothing", their_priority="Decor")
-# >       assert result
-# E       assert False
